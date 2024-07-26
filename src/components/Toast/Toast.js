f887cfa08e8b707363e955c6664eca7a419dd099
Freddie-Pike <freddiepike709@gmail.com>
2024-07-26 11:13:48 -0230

    Toast exercise 2, add Toast Component API.

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,16 +18,22 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
-  return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+function Toast({ children, variant = 'notice', isVisible = false, handleDismiss }) {
+  if (!Object.keys(ICONS_BY_VARIANT).includes(variant)) {
+    throw Error(`Invalid variant: ${variant}`);
+  }
+
+  const Icon = ICONS_BY_VARIANT[variant];
+
+  return isVisible && (
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon size={24} />
       </div>
       <p className={styles.content}>
-        16 photos have been uploaded
+        {children}
       </p>
-      <button className={styles.closeButton}>
+      <button className={styles.closeButton} onClick={handleDismiss}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+  const [toastVariant, setToastVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const handleDismiss = () => {
+    setIsVisible((state) => !state)
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      <Toast variant={toastVariant} isVisible={isVisible} handleDismiss={handleDismiss}>{message}</Toast>
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -24,7 +35,9 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" className={styles.messageInput} value={message} onChange={event => {
+              setMessage(event.target.value)
+            }} />
           </div>
         </div>
 
@@ -33,15 +46,21 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
+            {VARIANT_OPTIONS.map((variant) => (
+              <label htmlFor={`variant-${variant}`} key={variant}>
+                <input
+                  id={`variant-${variant}`}
+                  type="radio"
+                  name='variant'
+                  value={variant}
+                  checked={toastVariant === variant}
+                  onChange={event => {
+                    setToastVariant(event.target.value)
+                  }}
+                />
+                {variant}
+              </label>
+            ))}
 
             {/* TODO Other Variant radio buttons here */}
           </div>
@@ -52,7 +71,7 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={handleDismiss}>Pop Toast!</Button>
           </div>
         </div>
       </div>
